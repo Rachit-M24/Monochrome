e73450643d06e354b3564794aa95e39030cf48c3
Rachit Mahida <127675261+Rachit-M24@users.noreply.github.com>
2024-08-07 23:38:37 +0530

    Add files via upload

--- src/pages/ForeCast.jsx
+++ src/pages/ForeCast.jsx
@@ -3,18 +3,18 @@ import "../App.css";
 
 const Forecast = ({ forecast }) => {
   return (
-    <div className="bg-white  rounded-lg shadow-md w-[95%]">
-      <h2 className="text-2xl font-bold mb-4">7-Day Forecast</h2>
+    <div className="bg-white rounded-lg shadow-md w-[95%]">
+      <h2 className="text-2xl font-bold mb-4">5-Day Forecast</h2>
       <div className="space-y-4 w-full">
-        {forecast.list.slice(0, 5).map((day) => (
+        {forecast.list.filter(item => item.dt_txt.includes("12:00:00")).map((day) => (
           <div
             key={day.dt}
             className="span-box flex justify-between items-center"
           >
             <span className="weekend-span">
-              {new Date(day.dt_txt).toLocaleDateString()}
+              {new Date(day.dt_txt).toDateString()}
             </span>
-            <span className="weekend-span">{day.main.temp}°C </span>
+            <span className="weekend-span">{day.main.temp}°C</span>
             <span className="weekend-span">{day.weather[0].description}</span>
           </div>
         ))}
