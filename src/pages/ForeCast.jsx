fab1868f1fca7484a179959f4bce2a813fba9a32
Rachit Mahida <127675261+Rachit-M24@users.noreply.github.com>
2024-08-07 22:58:56 +0530

    Update ForeCast.jsx

--- src/pages/ForeCast.jsx
+++ src/pages/ForeCast.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import "../App.css";
 
 const Forecast = ({ forecast }) => {
-  const dailyForecasts = forecast.list.filter((item) =>
-    item.dt_txt.includes("12:00:00")
-  );
-
   return (
     <div className="bg-white  rounded-lg shadow-md w-[95%]">
       <h2 className="text-2xl font-bold mb-4">7-Day Forecast</h2>
       <div className="space-y-4 w-full">
-        {dailyForecasts.map((day) => (
+        {forecast.list.slice(0, 5).map((day) => (
           <div
             key={day.dt}
             className="span-box flex justify-between items-center"
@@ -18,7 +14,7 @@ const Forecast = ({ forecast }) => {
             <span className="weekend-span">
               {new Date(day.dt_txt).toLocaleDateString()}
             </span>
-            <span className="weekend-span">{day.main.temp}°C</span>
+            <span className="weekend-span">{day.main.temp}°C </span>
             <span className="weekend-span">{day.weather[0].description}</span>
           </div>
         ))}
